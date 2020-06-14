9ac86fec9acc8564073bd9f8a35c5c64cd782e5f
fendiya <f3ndi.354@gmail.com>
2020-06-15 00:51:33 +0700

    Add dice vector image, enable jatpack for support vector below version 21, optimize by using lateinit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,13 +3,39 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var rollImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener{Toast.makeText(this,"button_clicked",Toast.LENGTH_SHORT).show()}
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+
+        rollImage = findViewById(R.id.roll_image)
+    }
+
+    private fun rollDice() {
+//        val rollImage: ImageView = findViewById(R.id.roll_image)
+        val randomInt = Random().nextInt(6)+1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+
+        }
+        rollImage.setImageResource(drawableResource)
     }
 }
